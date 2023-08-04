35637de1ec99687a217533dc768a2486a9105a16
Thirumalaikumar-M <mthirumalaitk@gmail.com>
2023-08-04 11:41:09 +0530

    Add feed component

--- src/components/Feed.jsx
+++ src/components/Feed.jsx
@@ -1,7 +1,32 @@
-import React from "react";
+import { useState, useEffect } from "react";
+import { Box, Stack, Typography } from "@mui/material";
+import { Sidebar, Videos } from "./";
 
 const Feed = () => {
-  return <div>Feed</div>;
+  return (
+    <Stack sx={{ flexDirection: { sx: "column", md: "row" } }}>
+      <Box
+        sx={{
+          height: { sx: "auto", md: "92vh" },
+          borderRight: "1px solid #3d3d3d",
+          px: { sx: 0, md: 2 },
+        }}
+      >
+        <Sidebar />
+      </Box>
+      <Box p={2} sx={{ height: "90vh", overflowY: "auto", flex: 2 }}>
+        <Typography
+          variant="h4"
+          fontWeight="bold"
+          mb={2}
+          sx={{ color: "white" }}
+        >
+          New <span style={{ color: "#F31503" }}>videos</span>
+        </Typography>
+        <Videos />
+      </Box>
+    </Stack>
+  );
 };
 
 export default Feed;
